971279954266972975dfa072a8c8413be13e6741
Lenz Weber-Tronic <mail@lenzw.de>
2024-03-08 21:08:18 +0100

    remove install/uninstall code due to docker usage

--- xExtension-TTRSS_API/extension.php
+++ xExtension-TTRSS_API/extension.php
@@ -10,39 +10,13 @@ class TTRSS_APIExtension extends Minz_Extension {
 	}
 
 	public function install() {
-		$filename = 'ttrss.php';
-		$file_source = join_path($this->getPath(), $filename);
-		$path_destination = join_path(PUBLIC_PATH, 'api');
-		$file_destination = join_path($path_destination, $filename);
-
-		if (!is_writable($path_destination)) {
-			return 'server cannot write in ' . $path_destination;
-		}
-
-		if (file_exists($file_destination)) {
-			if (!unlink($file_destination)) {
-				return 'API file seems already existing but cannot be removed';
-			}
-		}
-
-		if (!file_exists($file_source)) {
-			return 'API file seems not existing in this extension. Try to download it again.';
-		}
-
-		if (!copy($file_source, $file_destination)) {
-			return 'the API file has failed during installation.';
-		}
+		// this is mounted in via docker, nothing to install
 
 		return true;
 	}
 
 	public function uninstall() {
-		$filename = 'ttrss.php';
-		$file_destination = join_path(PUBLIC_PATH, 'api', $filename);
-
-		if (file_exists($file_destination) && !unlink($file_destination)) {
-			return 'API file cannot be removed';
-		}
+		// this is mounted in via docker so it will be removed via docker - nothing to do
 
 		return true;
 	}
